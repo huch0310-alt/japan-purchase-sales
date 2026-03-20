d57a18621516485f5bc9de5bc2449fbb96466c87
huch03010 <huch03010@example.com>
2026-03-21 00:55:29 +0800

    fix: 修复TasksModule依赖

--- backend/src/common/services/tasks.module.ts
+++ backend/src/common/services/tasks.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TasksService } from './tasks.service';
+import { InvoicesModule } from '../../invoices/invoices.module';
+import { MessagesModule } from '../../messages/messages.module';
+import { SettingsModule } from '../../settings/settings.module';
 
 /**
  * 定时任务模块
  */
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), InvoicesModule, MessagesModule, SettingsModule],
   providers: [TasksService],
   exports: [TasksService],
 })
